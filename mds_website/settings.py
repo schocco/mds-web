1460c02c6a76d32b1293af38baa62964e95ceb06
Rocco Schulz <schocco.rulz@googlemail.com>
2013-06-11 21:59:12 +0200

    added new app with south migration folder.

--- mds_website/settings.py
+++ mds_website/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'south',
+    'apps.muni_scales',
 )
 
 # A sample logging configuration. The only tangible logging
