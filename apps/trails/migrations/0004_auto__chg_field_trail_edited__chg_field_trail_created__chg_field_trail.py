4ef6ef15d841a393f008ef7d4a206ba24c422e10
Rocco Schulz <schocco.rulz@googlemail.com>
2013-07-30 17:37:45 +0200

    add gpx file for unit test and south migration

---
+++ apps/trails/migrations/0004_auto__chg_field_trail_edited__chg_field_trail_created__chg_field_trail.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Trail.edited'
+        db.alter_column(u'trails_trail', 'edited', self.gf('django.db.models.fields.DateTimeField')(auto_now=True))
+
+        # Changing field 'Trail.created'
+        db.alter_column(u'trails_trail', 'created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+        # Changing field 'Trail.waypoints'
+        db.alter_column(u'trails_trail', 'waypoints', self.gf('django.contrib.gis.db.models.fields.LineStringField')(dim=3, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Trail.edited'
+        db.alter_column(u'trails_trail', 'edited', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+        # Changing field 'Trail.created'
+        db.alter_column(u'trails_trail', 'created', self.gf('django.db.models.fields.DateTimeField')())
+
+        # Changing field 'Trail.waypoints'
+        db.alter_column(u'trails_trail', 'waypoints', self.gf('django.contrib.gis.db.models.fields.LineStringField')(null=True))
+
+    models = {
+        u'trails.trail': {
+            'Meta': {'object_name': 'Trail'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'edited': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'waypoints': ('django.contrib.gis.db.models.fields.LineStringField', [], {'dim': '3', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['trails']
