760d654198ae8a4c1f156b108e63cd7a4fca3463
Rocco Schulz <schocco.rulz@googlemail.com>
2013-08-11 16:30:20 +0200

    make sure height profile canvas fills parent div in trail detail view

--- mds_website/static/js/app/views/TrailDetailView.js
+++ mds_website/static/js/app/views/TrailDetailView.js
@@ -8,6 +8,8 @@ define(['backbone',
 	
 	var TrailDetailView = Backbone.View.extend({
 		el: '#content',
+		
+		
 		initialize: function (options) {
 			var that = this;
 		    var onDataHandler = function(model) {
@@ -17,7 +19,6 @@ define(['backbone',
 		    that.id = options.id;
 		    that.trail = new Trail({id: that.id});
 		    that.trail.fetch({success: onDataHandler});
-		    //that.trail.on("reset", this.render, this);
 		},
 		
 		/**
@@ -29,20 +30,29 @@ define(['backbone',
 			var start = waypoints.coordinates[0][2] || 0;
 			var end = waypoints.coordinates[waypoints.coordinates.length-1][2] || 0;
 			
+			var dataset = [];
+			var labels = [];
+			
+			for(i=0;i<35;i++){
+				  dataset[i] = Math.floor((Math.random()*10)+200);
+				  labels[i] = i + " km";
+			}
+			
 			var data = {
-					labels : ["Start height","End height"],
+					labels : labels,//["Start height","End height"],
 					datasets : [
 						{
 							fillColor : "rgba(151,187,205,0.5)",
 							strokeColor : "rgba(151,187,205,1)",
 							pointColor : "rgba(151,187,205,1)",
 							pointStrokeColor : "#fff",
-							data : [start,end]
+							data : dataset //[start,end]
 						}
 					]
 				}
 			return data;
 		},
+		
 		
 		/**
 		 * Create and render an open layers map.
@@ -62,17 +72,40 @@ define(['backbone',
 			map.setCenter(new OpenLayers.LonLat(1, 2), 3);	
 		},
 		
+		
+		render_height_profile: function(){
+			var canvasdiv = document.getElementById("canvasdiv");
+			var canvas = document.getElementById("height_profile");
+			canvas.width = canvasdiv.clientWidth;
+			canvas.height = canvasdiv.clientHeight;		
+			var ctx = document.getElementById("height_profile").getContext("2d");
+			var data = this.get_chart_data();
+			// fixed scale
+			var options = {
+					scaleOverride : true,
+					scaleSteps : 10,
+					scaleStepWidth : 5,
+					scaleStartValue : 190,
+					pointDot : false,
+					scaleLabel : "<%=value%> m"
+				};
+			console.log("render hight profile.");
+			var myNewChart = new Chart(ctx).Line(data, options);
+		},
+		
+		/** renders the whole view. */
 		render: function(){
 			console.log("render template");
 			var compiledTemplate = _.template( tpl, {'trail': this.trail });
 			$(this.el).html(compiledTemplate);
-			var ctx = document.getElementById("height_profile").getContext("2d");
-			var data = this.get_chart_data();
-			var myNewChart = new Chart(ctx).Line(data);
+			
+			console.log("create hight profile");
+			this.render_height_profile();
 			
 			console.log("create map.");
 			this.render_map();
-		},
+			
+		}
 			
 	});
 	
