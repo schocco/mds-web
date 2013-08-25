42f325f520869979d4ad3be8a0e48b782b081367
Rocco Schulz <schocco.rulz@googlemail.com>
2013-08-25 23:58:16 +0200

    trailing slash for model urls

--- mds_website/static/js/app/models/BaseModel.js
+++ mds_website/static/js/app/models/BaseModel.js
@@ -4,6 +4,11 @@ define(['backbone'],
 		
 		prefix: "#/",
 		
+		url: function() {
+			var origUrl = Backbone.Model.prototype.url.call(this);
+			return origUrl + (origUrl.charAt(origUrl.length - 1) == '/' ? '' : '/');
+		},
+		
 		get_url: function(){
 			return this.prefix + this.get('id');
 		}
