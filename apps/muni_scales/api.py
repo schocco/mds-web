637cc8bbe5ea479105c7998daf99078f442483e7
Rocco Schulz <schocco.rulz@googlemail.com>
2013-09-13 17:12:08 +0200

    add api resource endpoint to get score of UDH object

--- apps/muni_scales/api.py
+++ apps/muni_scales/api.py
@@ -1,28 +1,15 @@
 # -*- coding: utf-8 -*-
+from apps.muni_scales.fields import MscaleField
 from apps.muni_scales.models import UDHscale, UXCscale
 from apps.muni_scales.mscale import Mscale, MSCALES
 from django.conf.urls import url
+from django.utils import simplejson
 from tastypie import fields
 from tastypie.authorization import Authorization
 from tastypie.bundle import Bundle
+from tastypie.fields import ApiField, CharField
 from tastypie.resources import Resource, ModelResource
 
-class UDHResource(ModelResource):
-    '''
-    UDH rating
-    '''
-    class Meta:
-        queryset = UDHscale.objects.all()
-        resource_name = 'udh-scale'
-    
-class UXCResource(ModelResource):
-    '''
-    UXC Rating
-    '''
-    class Meta:
-        queryset = UXCscale.objects.all()
-        resource_name = 'uxc-scale'    
-    
 class MscaleResource(Resource):
     '''
     A read-only Mscale resource.
@@ -66,3 +53,51 @@ class MscaleResource(Resource):
     def obj_get(self, request=None, **kwargs):
         pk = float(kwargs['pk'])
         return MSCALES[pk]
+
+class UDHResource(ModelResource):
+    '''
+    UDH rating
+    
+from apps.muni_scales.api import *
+from apps.muni_scales.models import UDHscale
+data={'maximum_difficulty':2,'average_difficulty':1, 'total_length':12, 'average_slope':20}
+
+scale = UDHResource()
+udh = UDHscale(**data)
+bundle = scale.build_bundle(udh)
+scale.full_dehydrate(bundle)
+    '''
+    maximum_difficulty = fields.ToOneField(MscaleResource, attribute="maximum_difficulty")
+    average_difficulty = fields.ToOneField(MscaleResource, attribute="average_difficulty")
+    score = fields.DictField(attribute='get_score', readonly=True, use_in="detail")
+    
+    
+    class Meta:
+        queryset = UDHscale.objects.all()
+        resource_name = 'udh-scale'
+        
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/calculate/$" % 
+                self._meta.resource_name, self.wrap_view('get_score'), name="api_dispatch_detail"),
+        ]
+        
+    def get_score(self, request, **kwargs):
+        '''
+        Return the score for the calculation
+        '''
+        scale = UDHResource()
+        #TODO: check input data
+        scale_bundle = scale.build_bundle(data=request.POST, request=request)
+        return self.create_response(request, scale_bundle)
+    
+class UXCResource(ModelResource):
+    '''
+    UXC Rating
+    '''
+    maximum_difficulty = fields.ToOneField(MscaleResource, attribute="maximum_difficulty")
+    average_difficulty = fields.ToOneField(MscaleResource, attribute="average_difficulty")
+    
+    class Meta:
+        queryset = UXCscale.objects.all()
+        resource_name = 'uxc-scale'    
